e38406c5fee2e7ba2a9be10cdf91833fbed0ceab
Veronika Chernikova <fotonikanvr@yandex.ru>
2025-02-07 12:35:40 +0300

    Домашняя работа 12_02_25

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,27 @@
+import logging
+
+logger = logging.getLogger('masks')
+logger.setLevel(logging.DEBUG)
+logger_hendler = logging.FileHandler('C:/Users/PROGRAMM/PycharmProjects/work/logs/masks.log', 'w')
+logger_formater = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
+logger_hendler.setFormatter(logger_formater)
+logger.addHandler(logger_hendler)
+
+
 def get_mask_card_numder(number_card: str) -> str:
     """Функция принимает номер карты и возвращает строку с ее маской"""
 
+    logger.info('Checking the correctness of the card number')
     if isinstance(number_card, str):
         if not number_card.isdigit():
+            logger.error('Format error')
             return "Номер карты не может содержать буквы!"
 
         if len(number_card) != 16:
+            logger.error('Number length error')
             return "Вы ввели некорректный номер карты!"
         else:
+            logger.info('Creating a card number mask')
             return f"{number_card[:4]} {number_card[4:6]}** **** {number_card[12:]}"
     else:
         return 'Введите номер в строчном формате'
@@ -16,13 +30,22 @@ def get_mask_card_numder(number_card: str) -> str:
 def get_mask_account(number_account: str) -> str:
     """Функция принимает номер счета и возвращает строку с его маской"""
 
+    logger.info('Checking the correctness of the account number')
     if isinstance(number_account, str):
         if not number_account.isdigit():
+            logger.error('Number length error')
             return "Номер счета не может содержать буквы!"
 
         if len(number_account) != 20:
+            logger.error('Number length error')
             return "Вы ввели некорректный номер счета!"
         else:
+            logger.info('Create an account number mask')
             return f"**{number_account[-4:]}"
     else:
         return 'Введите номер в строчном формате'
+
+
+if __name__ == '__main__':
+    print(get_mask_card_numder('123456789123456'))
+    print(get_mask_account('12345678912345678912'))
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 import json
+import logging
+
+logger = logging.getLogger('utils')
+logger.setLevel(logging.DEBUG)
+logger_hendler = logging.FileHandler('C:/Users/PROGRAMM/PycharmProjects/work/logs/utils.log', 'w')
+logger_formater = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
+logger_hendler.setFormatter(logger_formater)
+logger.addHandler(logger_hendler)
 
 
 def get_data_json(way_file="") -> list:
@@ -7,9 +15,16 @@ def get_data_json(way_file="") -> list:
     функция возвращает пустой список."""
     data = []
     try:
+        logger.info('Open and read the file with transactions')
         with open(way_file, encoding='utf-8') as file:
             data = json.load(file)
-    except Exception:
+        logger.info('Transactions were read successfully')
+    except Exception as e:
+        logger.error(f'error: {e}')
         return data
     finally:
         return data
+
+
+if __name__ == '__main__':
+    get_data_json('C:/Users/PROGRAMM/PycharmProjects/work/data/operation.json')
